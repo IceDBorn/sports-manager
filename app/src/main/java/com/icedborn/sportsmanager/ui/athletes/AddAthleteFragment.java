d958592b34821e988493827f6e099047091ac4ef
IceDBorn <fundwork@tutanota.com>
2021-05-12 20:24:09 +0300

    Add spinner control - add save button

--- app/src/main/java/com/icedborn/sportsmanager/ui/athletes/AddAthleteFragment.java
+++ app/src/main/java/com/icedborn/sportsmanager/ui/athletes/AddAthleteFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,12 +15,17 @@ import androidx.fragment.app.Fragment;
 
 import com.icedborn.sportsmanager.R;
 import com.icedborn.sportsmanager.controllers.DateController;
+import com.icedborn.sportsmanager.ui.sports.SportModel;
+import com.icedborn.sportsmanager.ui.sports.SportsViewModel;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AddAthleteFragment extends Fragment {
     private DatePickerDialog datePickerDialog;
-    TextView dateButton;
+    TextView date;
+    Spinner spinner;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,11 +34,21 @@ public class AddAthleteFragment extends Fragment {
 
         InitDatePicker();
 
-        dateButton = root.findViewById(R.id.athleteBirthDate);
+        date = root.findViewById(R.id.athleteBirthDate);
 
-        dateButton.setText(DateController.getToday());
+        date.setText(DateController.getToday());
 
-        dateButton.setOnClickListener(v -> datePickerDialog.show());
+        date.setOnClickListener(v -> datePickerDialog.show());
+
+        spinner = root.findViewById(R.id.addAthleteSports);
+
+        String[] sports = {"One", "Two", "Three"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this.getContext(), android.R.layout.simple_spinner_item, sports);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
 
         return root;
     }
@@ -40,12 +57,12 @@ public class AddAthleteFragment extends Fragment {
         DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
             month = month + 1;
             String date = DateController.makeDateString(dayOfMonth, month, year);
-            dateButton.setText(date);
+            this.date.setText(date);
         };
 
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
-        int month  = calendar.get(Calendar.MONTH);
+        int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         int style = AlertDialog.THEME_HOLO_DARK;
